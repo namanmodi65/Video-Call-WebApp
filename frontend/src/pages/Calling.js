0bbc2930938b5f5174d8d321d59aad25a3ad641c
Naman Jain <103953760+namanmodi65@users.noreply.github.com>
2023-12-30 23:04:49 +0530

    complete

--- frontend/src/pages/Calling.js
+++ frontend/src/pages/Calling.js
@@ -7,6 +7,9 @@ function Calling() {
   const socket = useSocket()
   const [remoteSocketId,setRemoteSocketId] = useState(null)
   const [myStream,setMyStream] = useState()
+  const [remoteStream,setRemoteStream] = useState()
+  
+  
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined room`);
     setRemoteSocketId(id);
@@ -25,31 +28,79 @@ function Calling() {
       const ans = await peer.getAnswer(offer)
       socket.emit('call:accepted',{to:from,ans})
     },
-    [],
+    [socket],
   )
   
+
+  const sendStreams = useCallback(() => {
+    for (const track of myStream.getTracks()) {
+      peer.peer.addTrack(track, myStream);
+    }
+  }, [myStream]);
+
+
 
   const handleCallAccepted=useCallback(
     async({from,ans}) => {
       peer.setLocalDescription(ans)
-      console("Call aCCEPTED")
+      console.log("Call aCCEPTED")
+      sendStreams()
     },
-    [],
+    [sendStreams],
   )
   
+  const handleNegoNeeded = useCallback(async () => {
+    const offer = await peer.getOffer();
+    socket.emit("peer:nego:needed", { offer, to: remoteSocketId });
+  }, [remoteSocketId, socket]);
+
+  useEffect(() => {
+    peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
+    return () => {
+      peer.peer.removeEventListener("negotiationneeded", handleNegoNeeded);
+    };
+  }, [handleNegoNeeded]);
+
+  const handleNegoNeedIncomming = useCallback(
+    async ({ from, offer }) => {
+      const ans = await peer.getAnswer(offer);
+      socket.emit("peer:nego:done", { to: from, ans });
+    },
+    [socket]
+  );
+  
+  const handleNegoNeedFinal = useCallback(async ({ ans }) => {
+    await peer.setLocalDescription(ans);
+  }, []);
+
+  
+
+  useEffect(() => {
+    peer.peer.addEventListener("track", async (ev) => {
+      const remoteStream = ev.streams;
+      console.log("GOT TRACKS!!");
+      setRemoteStream(remoteStream[0]);
+    });
+  }, []);
+  
+
 
 
   useEffect(() => {
     socket.on("user:joined",handleUserJoined)
     socket.on('incomming:call',handleIncommingCall)
     socket.on('call:accepted',handleCallAccepted)
+    socket.on('peer:nego:needed',handleNegoNeedIncomming)
+    socket.on('peer:nego:final',handleNegoNeedFinal)
 
     return ()=>{
       socket.off('user:joined',handleUserJoined)
       socket.off('incomming:call',handleIncommingCall)
       socket.off('call:accepted',handleCallAccepted)
+      socket.off('peer:nego:needed',handleNegoNeedIncomming)
+      socket.off('peer:nego:final',handleNegoNeedFinal)
     }
-  }, [socket,handleUserJoined,handleIncommingCall,handleCallAccepted])
+  }, [socket,handleUserJoined,handleIncommingCall,handleCallAccepted,handleNegoNeedIncomming,handleNegoNeedFinal])
   
   const handleCallUser=useCallback(
     async() => {
@@ -66,21 +117,36 @@ function Calling() {
   
 
   return (
-    <>
-      <h1>Calling</h1>
-      <h3>{remoteSocketId ? 'Connected':'No one in room'}</h3>
-      {
-        remoteSocketId && <button onClick={handleCallUser}>Call</button>
-      }
-      {
-        
-        myStream && (<>
-        <h1>My Stream</h1>
-         <ReactPlayer playing muted height='300px' width='400px' url={myStream} />
-         </>
-         )
-      }
-    </>
+    <div>
+      <h1>Room Page</h1>
+      <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
+      {myStream && <button onClick={sendStreams}>Send Stream</button>}
+      {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
+      {myStream && (
+        <>
+          <h1>My Stream</h1>
+          <ReactPlayer
+            playing
+            muted
+            height="100px"
+            width="200px"
+            url={myStream}
+          />
+        </>
+      )}
+      {remoteStream && (
+        <>
+          <h1>Remote Stream</h1>
+          <ReactPlayer
+            playing
+            muted
+            height="100px"
+            width="200px"
+            url={remoteStream}
+          />
+        </>
+      )}
+    </div>
   )
 }
 
